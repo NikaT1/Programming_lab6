2404f465aa9ee77919c41a2cc1490504764b31d1
nika <troynikv@gmail.com>
2024-07-22 17:28:14 +0300

    Remove redundant request model

--- Server/src/main/java/com/console_project/server/connection/Request.java
+++
@@ -1,8 +0,0 @@
-package com.console_project.server.connection;
-
-import com.console_project.shared.connection_model.UserRequest;
-
-import java.net.InetAddress;
-
-public record Request<T>(UserRequest<T> userRequest, int port, InetAddress addr) {
-}
